af64a08491c6006b086f5a0cdbf04e4226e5fe89
Phionanamugga <phionanamugga15@gmail.com>
2025-01-03 18:06:02 +0100

    enables admin edit questions for the polls[Delivers##5328619]

--- appentry/pollapp/admin.py
+++ appentry/pollapp/admin.py
@@ -3,13 +3,21 @@ from django.contrib import admin
 from .models import Choice, Question
 
 
-class QuestionAdmin(admin.ModelAdmin):
-    fieldsets = [
-        (None, {"fields": ["question_text"]}),
-        ("Date information", {"fields": ["pub_date"]}),
-    ]
-
-
 admin.site.register(Question, QuestionAdmin)
 # ...
 admin.site.register(Choice)
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["pub_date"], "classes": ["collapse"]}),
+    ]
+    inlines = [ChoiceInline]
+
+
+admin.site.register(Question, QuestionAdmin)
